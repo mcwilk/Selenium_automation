8c1f1967992c2f94fe214f6137a6c4727b84ceed
mcwilk <maciej.wilk04@gmail.com>
2020-10-25 13:02:32 +0100

    Initial commit v2

---
+++ utils/search_data.py
@@ -0,0 +1,20 @@
+class SearchRegistrationData:
+
+    def __init__(self, title, fname, lname, email, passwd, birth, company,
+                 address, city, state, post, country, phone):
+        self.title = title
+        self.fname = fname
+        self.lname = lname
+        self.email = email
+        self.passwd = passwd
+        self.birth = birth
+        self.company = company
+        self.address = address
+        self.city = city
+        self.state = state
+        self.post = post
+        self.country = country
+        self.phone = phone
+
+
+
